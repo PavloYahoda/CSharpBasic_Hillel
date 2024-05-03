c948692a4fefc7ebd77d811fddda0b51c211b3d2
Pavlo Yahoda <pavlo.yahoda@gmail.com>
2024-05-03 11:50:58 +0300

    HomeWork9+

---
+++ Lesson9/OOP_Books/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OOP_Books
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+        private int yearOfPublication;
+        private int numberOfPages;
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int YearOfPublication { get; set; }
+        public int NumberOfPages { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Title: {Title}\n" +
+                $"Author: {Author}\n" +
+                $"YearOfPublication: {YearOfPublication}\n" +
+                $"NumberOfPages: {NumberOfPages}");
+        }
+        public void IsThick()
+        {
+            if (NumberOfPages > 500)
+            {
+                Console.WriteLine("This book is thick!");
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("This book is not so thick!");
+                Console.WriteLine("");
+            }
+        }
+    }
+}
