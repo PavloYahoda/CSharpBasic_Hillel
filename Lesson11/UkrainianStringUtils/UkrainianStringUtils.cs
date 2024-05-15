70d21633e7922905d58330015011f40ab14ab801
Pavlo Yahoda <pavlo.yahoda@gmail.com>
2024-05-15 21:40:35 +0300

    HomeWork11_part1

---
+++ Lesson11/UkrainianStringUtils/UkrainianStringUtils.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+
+namespace UkrainianStringUtilsLib
+{
+    public static class UkrainianStringUtils
+    {
+        public static int CountVowels(string input)
+        {
+            string allVowels = "аеєиіїоуюяAEЄИIЇОУЮЯ";
+
+            int count = 0;
+            foreach (char ch in input)
+            {
+                if (allVowels.Contains(ch))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public static int CountConsonants(string input)
+        {
+            // Стрінг з приголосними літерами
+            string allConsonants = "бвгґджзклмнпрстфхцчшщБВГҐДЖЗКЛМНПРСТФХЦЧШЩ";
+
+            int count = 0;
+            foreach (char ch in input)
+            {
+                if (allConsonants.Contains(ch))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public static string ReverseString(string input)
+        {
+            return new string(input.ToCharArray().Reverse().ToArray());
+        }
+        public static string RemoveDuplicates(string input)
+        {
+            return new string(input.Distinct().ToArray());
+        }
+        public static string RemovePunctuation(string input)
+        {
+            return Regex.Replace(input, @"[\p{P}\p{S}\s]", "");
+        }
+    }
+}
---
+++ Lesson11/WorkWithString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UkrainianStringUtilsLib;
+
+namespace WorkWithString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            Console.Write("Введіть будь-яке речення: ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine($"Кількість голосних у реченні: {UkrainianStringUtils.CountVowels(input)}");
+            Console.WriteLine($"Кількість приголосних у реченні: {UkrainianStringUtils.CountConsonants(input)}");
+            Console.WriteLine($"Речення навпаки: {UkrainianStringUtils.ReverseString(input)}");
+            Console.WriteLine($"Без дублікатів: {UkrainianStringUtils.RemoveDuplicates(input)}");
+            Console.WriteLine($"Без пунктуації: {UkrainianStringUtils.RemovePunctuation(input)}");
+
+            Console.ReadKey();
+        }
+    }
+}
