112e27931665566b01e838f85bdae8dde1ab82ea
Pavlo Yahoda <pavlo.yahoda@gmail.com>
2024-05-16 15:40:41 +0300

    HomeWork11

---
+++ Lesson11/WorkWithExceptions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 0;
+            Console.Write("Enter your age: ");
+            try
+            {
+                age = Convert.ToInt32(Console.ReadLine());//default message
+                if (age <= 0) throw new FormatException("Age should be greater than zero");//custom message
+                Console.WriteLine($"Your age is {age}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Message: {0}", ex.Message);
+            }
+            Console.WriteLine("The program is still running");
+            Console.ReadKey();
+        }
+    }
+}
