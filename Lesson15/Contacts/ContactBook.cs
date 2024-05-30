5a07872bfa0e0f6b2dab6569e018119ee58c79ee
Pavlo Yahoda <pavlo.yahoda@gmail.com>
2024-05-30 12:15:07 +0300

    HomeWork15

---
+++ Lesson15/Contacts/ContactBook.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts
+{
+    public class ContactBook
+    {
+        public static Dictionary<string, string> contacts = new Dictionary<string, string>() {
+            {"Pavlo", "0123456789"},
+            {"Bogdan", "0123364455"},
+            {"Andrii", "0123894434"},   
+        };
+        
+        public void AddContact(string name, string phone) {
+            contacts.Add(name, phone);
+            Console.WriteLine("New contact is saved");
+        }
+        public void RemoveContact(string name)
+        {
+            bool isExist = false;
+            if (contacts.ContainsKey(name)) {
+                contacts.Remove(name);
+                isExist = true;
+            }
+            Console.WriteLine(isExist ? $"{name} is deleted from Contacts" : $"There is no {name} in contacts");
+        }
+        public void ChangeContact(string name)
+        {
+            bool isExistForGhange = false;
+            if (contacts.ContainsKey(name)) {
+                Console.Write("Enter new phone number: ");
+                contacts[name] = Console.ReadLine();
+                isExistForGhange = true;
+            }
+            Console.WriteLine(isExistForGhange ? $"{name} is changed" : $"There is no {name} in contacts");
+        }
+        public void FindContact(string name)
+        {
+            bool isFound = false;
+            if (contacts.ContainsKey(name)) {
+                isFound = true;
+            }
+            Console.WriteLine(isFound ? $"{name}: {contacts[name]}" : $"There is no {name} in contacts");
+        }
+        public void GetAllContacts()
+        {
+            foreach (var item in contacts)
+            {
+                Console.Write($"{item.Key}:\t {item.Value}\n");
+            }
+        }
+    }
+}
--- Lesson15/Contacts/Program.cs
+++ Lesson15/Contacts/Program.cs
@@ -10,6 +10,59 @@ namespace Contacts
     {
         static void Main(string[] args)
         {
+                int userChoice;
+                ContactBook cb = new ContactBook();
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Main menu:\n" +
+                        "1. Add contact\n" +
+                        "2. Remove contact\n" +
+                        "3. Edit contact\n" +
+                        "4. Find contact\n" +
+                        "5. Show all contacts\n" +
+                        "0. For Exit");
+
+                    Console.Write("Please, make your choice: ");
+                    userChoice = Helper.ReadChoice();
+                    Console.Clear();
+                    switch (userChoice)
+                    {
+                        case 1:
+                            Console.Write("Enter name: ");
+                            string nameForAdding = Console.ReadLine();
+                            Console.Write("Enter phone number: ");
+                            string phoneForAdding = Console.ReadLine();
+                            cb.AddContact(nameForAdding, phoneForAdding);
+                            Console.ReadKey();
+                            break;
+                        case 2:
+                            Console.Write("Enter name: ");
+                            string nameForRemoving = Console.ReadLine();
+                            cb.RemoveContact(nameForRemoving);
+                            Console.ReadKey();
+                            break;
+                        case 3:
+                            Console.Write("Enter name: ");
+                            string nameForChanging = Console.ReadLine();
+                            cb.ChangeContact(nameForChanging);
+                            Console.ReadKey();
+                            break;
+                        case 4:
+                            Console.Write("Enter name: ");
+                            string nameForSearching = Console.ReadLine();
+                            cb.FindContact(nameForSearching);
+                            Console.ReadKey();
+                            break;
+                        case 5:
+                            cb.GetAllContacts();
+                            Console.ReadKey();
+                            break;
+                        case 0:
+                            Environment.Exit(0);
+                            break;
+                    }
+            } while (true);
         }
     }
 }
